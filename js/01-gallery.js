4352916574be376fc3f700a0c1357f18ace5e59e
jokerav <50701756+jokerav@users.noreply.github.com>
2022-01-26 19:18:07 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+//console.log(galleryItems);
+galleryItems.map((img) => {
+  gallery.insertAdjacentHTML(
+    "beforeend",
+    `
+    <div class="gallery__item">
+      <a class="gallery__link" href=${img.original}>
+        <img
+          class="gallery__image"
+          src=${img.preview}
+          data-source=${img.original}
+          alt=${img.description}
+        />
+      </a>
+    </div>`
+  );
+});
