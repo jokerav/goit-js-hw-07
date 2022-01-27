e69a95d4c0364c614b18005aab0649eda91f6aab
jokerav <50701756+jokerav@users.noreply.github.com>
2022-01-27 22:41:26 +0200

    начал второе задание

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+createGalleryMarkup();
+
+function createGalleryMarkup() {
+  galleryItems.map((img) => {
+    gallery.insertAdjacentHTML(
+      "beforeend",
+      `<a class="gallery__item" href=${img.original}>
+  <img class="gallery__image" src=${img.preview} alt=${img.description} />
+</a>`
+    );
+  });
+}
